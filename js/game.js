c26ff3479d226d4b6a11371d795d8e11f2a9a5b5
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-04-21 18:59:11 -0400

    fixed js

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@
 // Created on: April 2022
 // This is the Phaser3 configuration file
 
-import SplashScene from './splashScene.js'
+import SplashScene from './js/splashScene.js'
 
 // our game scene
 const slpashScene = new SplashScene()
