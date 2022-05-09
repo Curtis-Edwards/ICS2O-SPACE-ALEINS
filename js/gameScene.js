19dd4b69bb28f941229eab7ebacecbe02d9c2d04
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-09 13:13:07 -0400

    added ship phisicys

--- js/gameScene.js
+++ js/gameScene.js
@@ -32,8 +32,25 @@ class GameScene extends Phaser.Scene {
   }
 
   update(time, delta) {
-  }
+    // called 60 times a second, hopefully
 
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x > 0) {
+        this.ship.x > 0
+      }
+    }
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x > 0) {
+        this.ship.x > 0
+      }
+    }
+  }
 }
 
 export default GameScene
